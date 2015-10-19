6eeecc028e735d0a5a9175fc2d4bde71c7387627
Sébastien Vitard <contact@sebastienvitard.name>
2015-10-19 14:46:55 +0200

    Move static configuration getters and controller initializer methods into Cobalt, Add Cobalt font manager, Add some classes for native bars (it is coming)

--- Cobalt/CobaltAbstractPlugin.h
+++ Cobalt/CobaltAbstractPlugin.h
@@ -1,19 +1,49 @@
-//
-//  CobaltAbstractPlugin.h
-//  Cobalt
-//
-//  Created by Haploid on 23/07/14.
-//  Copyright (c) 2014 Haploid. All rights reserved.
-//
+/**
+ *
+ * CobaltAbstractPlugin.h
+ * Cobalt
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Cobaltians
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
 
 #import <Foundation/Foundation.h>
+
 #import "CobaltViewController.h"
 
 @interface CobaltAbstractPlugin : NSObject
 
-@property (nonatomic, retain) NSMutableArray * viewControllersArray;
+@property (nonatomic, retain) NSMutableArray *viewControllersArray;
 
-+ (CobaltAbstractPlugin *)sharedInstanceWithCobaltViewController: (CobaltViewController *)viewController;
-- (void)onMessageFromCobaltController:(CobaltViewController *)viewController andData: (NSDictionary *)data;
-- (void)onMessageFromWebLayerWithCobaltController:(CobaltViewController *)viewController andData: (NSDictionary *)data;
+/*!
+ @method    + (CobaltAbstractPlugin *)sharedInstanceWithCobaltViewController:(CobaltViewController *)viewController
+ @abstract  Returns the singleton instance of a Cobalt plugin.
+ @result    The singleton instance of a Cobalt plugin.
+ */
++ (CobaltAbstractPlugin *)sharedInstanceWithCobaltViewController:(CobaltViewController *)viewController;
+- (void)onMessageFromCobaltController:(CobaltViewController *)viewController
+                              andData:(NSDictionary *)data;
+- (void)onMessageFromWebLayerWithCobaltController:(CobaltViewController *)viewController
+                                          andData:(NSDictionary *)data;
+
 @end
