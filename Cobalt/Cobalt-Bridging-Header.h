ef60634691915a7ce847f155ac4729435067f5fe
AntoineBarroux <antoine.barroux@gmail.com>
2016-10-12 11:35:07 +0200

    Add swift support header file

---
+++ Cobalt/Cobalt-Bridging-Header.h
@@ -0,0 +1,27 @@
+//
+//  Cobalt-Bridging-Header.h
+//  Cobalt
+//
+//  Created by antoine on 12/10/2016.
+//  Copyright © 2016 Cobaltians. All rights reserved.
+//
+
+#ifndef Cobalt_Bridging_Header_h
+#define Cobalt_Bridging_Header_h
+
+#import "BackBarButtonItem.h"
+#import "Cobalt-Prefix.pch"
+#import "Cobalt.h"
+#import "CobaltAbstractPlugin.h"
+#import "CobaltAlert.h"
+#import "CobaltBarButtonItem.h"
+#import "CobaltButton.h"
+#import "CobaltFont.h"
+#import "CobaltFontManager.h"
+#import "CobaltPluginManager.h"
+#import "CobaltToast.h"
+#import "CobaltViewController.h"
+#import "ImageManager.h"
+#import "iToast.h"
+
+#endif /* Cobalt_Bridging_Header_h */
