ce4be84b3ea196ea4bf7009e6f500f549e7319b9
Guillaume Gendre <dzb.rtz@gmail.com>
2018-04-13 11:57:52 +0200

    removed some strong references to the webview to avoid potential leaks

--- Cobalt/CobaltViewController.h
+++ Cobalt/CobaltViewController.h
@@ -231,7 +231,7 @@ typedef NSInteger WebViewType;
  @property		webView
  @abstract		the webView displaying content
  */
-@property (strong, nonatomic) IBOutlet UIWebView * webView;
+@property (weak, nonatomic) IBOutlet UIWebView * webView;
 
 /*!
  @property		pageName
@@ -246,7 +246,7 @@ typedef NSInteger WebViewType;
  */
 @property (strong, nonatomic) NSDictionary *navigationData;
 
-@property (strong, nonatomic) IBOutlet UIWebView * webLayer;
+@property (weak, nonatomic) IBOutlet UIWebView * webLayer;
 
 /*!
  @property             refreshControl
